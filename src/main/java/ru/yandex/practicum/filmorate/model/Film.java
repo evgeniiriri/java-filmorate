1dca31d7019428d2d86f6df389833fa77ee829d7
evgeniiriri <100759731+evgeniiriri@users.noreply.github.com>
2024-08-27 18:45:31 +0300

    Revert "сделано"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,24 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
-import lombok.Data;
-
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Film.
  */
-@Data
+@Getter
+@Setter
 public class Film {
-    private Long id;
-    @NotBlank
-    @NotNull
-    private String name;
-    @NotBlank
-    @NotNull
-    @Size(max = 200)
-    private String description;
-    private LocalDate releaseDate;
-    @Positive
-    private int duration;
 }
