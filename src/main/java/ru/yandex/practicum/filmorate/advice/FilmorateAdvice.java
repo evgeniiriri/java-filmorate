eef6422c90fd5bdb14f55abb9b78518667c01bf0
evgeniiriri <evgenii.ridberg@yandex.ru>
2024-09-28 18:36:07 +0300

    рефактор

--- src/main/java/ru/yandex/practicum/filmorate/advice/FilmorateAdvice.java
+++ src/main/java/ru/yandex/practicum/filmorate/advice/FilmorateAdvice.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.advice;
 
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,8 +18,8 @@ public class FilmorateAdvice {
         return new ResponseEntity<>(new ErrorMessage(e.getMessage(), LocalDateTime.now(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(FilmorateValidationException.class)
-    public ResponseEntity<ErrorMessage> validationExceptionHandler(FilmorateValidationException e) {
+    @ExceptionHandler({FilmorateValidationException.class, ValidationException.class})
+    public ResponseEntity<ErrorMessage> validationExceptionHandler(RuntimeException e) {
         return new ResponseEntity<>(new ErrorMessage(e.getMessage(), LocalDateTime.now(), HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
     }
 
